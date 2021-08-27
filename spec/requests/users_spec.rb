4e5d6d8ac6fcd6b10e11ab7065510552b556d1eb
rushika1503 <rsirigadde_c@rpxcorp.com>
2021-08-27 11:02:28 +0530

    Rspec is done for registration

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe "Users", type: :request do
       expect(response).to have_http_status(:created)
     end
 
-    # it 'Registers the user with invalid params' do
-    #   post '/users', params: { name: 'userfour' ,username: 'userfour', password: '123456', password_confirmation:'123456' } 
-    #   puts(response.body)
-    #   expect(response).to have_http_status(:unprocessable_entity)
-    # end
+    it 'Registers the user with invalid params' do
+      post '/users', params: { name: 'userfour' ,username: 'userfour', password: '123456', password_confirmation:'123456' } 
+      puts(response.body)
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
   end
 end
