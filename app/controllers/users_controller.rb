0ff56e116bd70839df647761f512be6f5489a5be
rushika1503 <rsirigadde_c@rpxcorp.com>
2021-09-13 16:35:03 +0530

    first Heroku deploy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,17 @@
 class UsersController < ApplicationController
     before_action :authorize_request, except: :create
-  
+    
+
     def create
       result = Users::Registration.run(user_params)
       if result.success?
         render json: result.fixtures['user']
       else
         render json: { error: result.message }, status: :unauthorized
+        
       end
     end
+  
     
     def user_params
       params.permit(
--- app/domains/users/registration.rb
+++ app/domains/users/registration.rb
@@ -1,6 +1,6 @@
 module Users
     class Registration < Actionable::Action
-      
+
       step :build
       step :validate
       step :create_user
@@ -14,18 +14,23 @@ module Users
         @user = User.new @_params
       end
      
-      def validate
+       def validate
         fail :name_invalid, "Name missing" unless @_params[:name].present?
         fail :username_invalid, "UserName missing" unless @_params[:username].present?
         fail :email_invalid, "email missing" unless @_params[:email].present?
-        fail :pass_invalid, "pass missing" unless @_params[:password].present?
-        fail :pasconf_invalid, "password conf missing" unless @_params[:password_confirmation].present?  
+        fail :pass_invalid, "password missing" unless @_params[:password].present?
+        fail! :pasconf_invalid, "password conffirmation is missing" unless @_params[:password_confirmation].present?  
       end
-
+     
       def create_user
-        @user.save!
+        result = @user.save
+        unless result
+        fail!(
+        :invalid,
+        @user.errors.full_messages.join(", ")
+        ) 
+        end
       end
 
-      
     end
   end
